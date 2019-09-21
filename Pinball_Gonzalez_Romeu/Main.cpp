54aa40697f7a9160b73ae6431519bf6460736ebc
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-09-21 11:08:12 +0200

    SDL Included

--- Pinball_Gonzalez_Romeu/Main.cpp
+++ Pinball_Gonzalez_Romeu/Main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include <Box2D/Box2D.h>
+#include "SDL/include/SDL.h"
+#include "SDL_image/include/SDL_image.h"
+#include "SDL_mixer/include/SDL_mixer.h"
+#pragma comment(lib, "SDL/SDL2main.lib")
+#pragma comment(lib, "SDL/SDL2.lib")
+#pragma comment(lib, "SDL_Image/SDL2_image.lib")
+#pragma comment(lib, "SDL_mixer/SDL2_mixer.lib")
+#pragma comment(lib, "Box2D/lib/Box2D.lib")
 
 int main()
 {
